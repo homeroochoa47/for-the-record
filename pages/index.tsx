48893f6a690d3a990a297f61de936767fd5c3fd3
Homero Ochoa <homeroochoa47@gmail.com>
2023-09-11 15:17:57 -0700

    update login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,30 +10,17 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-background">
       <header>
 				<UserButton afterSignOutUrl="/"/>
 			</header>
 
-      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 bg-background">
+      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 bg-muted/50">
+        
         {/* Left side */}
-        <div className="relative hidden h-full flex-col bg-[url(/bg.svg)] p-10 text-white dark:border-r lg:flex">
+        <div className="relative hidden h-full flex-col bg-[url(/bg.svg)] p-10 text-foreground border-foreground dark:border-r lg:flex">
           <div className="absolute inset-0" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            For the Record
-          </div>
+            <img src="/for-the-record.svg" className="w-[400px]" alt="for the record logo" />
         </div>
 
         {/* Right side */}
